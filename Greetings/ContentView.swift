f6071cea713cfbfbd4d57338040d9ff1c35fede1
neomond <neoweise@yahoo.com>
2024-07-04 14:27:48 +0400

    adding custom views

--- Greetings/ContentView.swift
+++ Greetings/ContentView.swift
@@ -9,19 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Greetings")
-            .font(.largeTitle)
-            .fontWeight(.semibold)
-            .foregroundStyle(Color.purple)
-            .padding()
-            .background(
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(Color.orange.opacity(0.4))
-            )
-            .shadow(color: Color.orange.opacity(0.4), radius: 5, x: 10, y: 10)
+        VStack(alignment: .leading) {
+            TextView(text: "Hello!", color: .green)
+            TextView(text: "Hello, there!", color: .yellow)
+            TextView(text: "Welcome to Swift programming!", color: .red)
+            TextView(text: "Good luck!", color: .purple)
+            TextView(text: "Boom!", color: .gray)
+            TextView(text: "Are you ready to explore?", color: .blue)
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct TextView: View {
+    let text: String
+    let color: Color
+    
+    var body: some View {
+        Text(text)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .foregroundStyle(Color.white)
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(color.opacity(0.8))
+            )
+            .shadow(color: color.opacity(0.4), radius: 5, x: 10, y: 10)
+    }
 }
